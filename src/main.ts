8b860c4bd201df04e26a47b6885da0b3d7cba7b9
hamed elahifar <hamed.elahifar@gmail.com>
2024-03-10 21:07:47 +0330

    cors more changes

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,17 @@ async function bootstrap() {
     // logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
 
-  app.enableCors(); // Enable CORS
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
