f7ecc29d053d11ba768925bd252d535b11005dbd
hamed elahifar <hamed.elahifar@gmail.com>
2024-03-10 21:33:04 +0330

    add OPTIONS to cors

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,10 @@ async function bootstrap() {
 
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    );
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
   });
