d0eba87bd1d436f4e223acc646431a9de4e1a6dc
hamed elahifar <hamed.elahifar@gmail.com>
2024-03-11 12:05:12 +0330

    remove cors options

--- src/main.ts
+++ src/main.ts
@@ -27,25 +27,26 @@ async function bootstrap() {
   // );
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: false,
     // bufferLogs: true,
     // logger: false,
     // logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-    );
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header(
+  //     'Access-Control-Allow-Methods',
+  //     'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+  //   );
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
 
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-  });
+  // app.enableCors({
+  //   allowedHeaders: '*',
+  //   origin: '*',
+  // });
 
   app.useGlobalPipes(
     new ValidationPipe({
